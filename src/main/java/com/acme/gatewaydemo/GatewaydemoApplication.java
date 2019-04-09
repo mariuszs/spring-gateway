b7358c2dc170beb912e8c958247bd8d083821c55
Mariusz Smykula <mariuszs@gmail.com>
2019-04-09 17:20:44 +0200

    init

---
+++ src/main/java/com/acme/gatewaydemo/GatewaydemoApplication.java
@@ -0,0 +1,13 @@
+package com.acme.gatewaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewaydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewaydemoApplication.class, args);
+	}
+
+}
